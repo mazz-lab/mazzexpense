22ae51cdf4b047ef1fa6a8f993a7f84e7276aec8
mazzinfo <mazztechinfo@gmail.com>
2019-01-25 15:00:41 +0530

    category Db added

--- src/pages/addcategory/addcategory.ts
+++ src/pages/addcategory/addcategory.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angular';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { Toast } from '@ionic-native/toast';
 
 /**
  * Generated class for the AddcategoryPage page.
@@ -15,11 +17,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddcategoryPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  titlename:string;
+  categorydata = { categorytype:"", categoryname:""};
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl : ViewController,
+    private sqlite: SQLite,
+    private toast: Toast,) {
   }
 
+
+  public closeModal(){
+    this.viewCtrl.dismiss();
+}
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddcategoryPage');
+    console.log(this.navParams.get('message'));
+    this.titlename=this.navParams.get('pagename');
+  }
+
+  saveData() {
+
+   
+
+    this.sqlite.create({
+      name: 'mebdb.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('INSERT INTO expense VALUES(NULL,?,?,?,?)',[this.categorydata.categorytype,this.categorydata.categoryname])
+        .then(res => {
+          console.log(res);
+          this.toast.show('Data saved', '5000', 'center').subscribe(
+            toast => {
+              this.navCtrl.popToRoot();
+            }
+          );
+        })
+        .catch(e => {
+          console.log(e);
+          this.toast.show(e, '5000', 'center').subscribe(
+            toast => {
+              console.log(toast);
+            }
+          );
+        });
+    }).catch(e => {
+      console.log(e);
+      this.toast.show(e, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+    });
   }
 
 }
--- src/pages/category/category.ts
+++ src/pages/category/category.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ModalController } from 'ionic-angular';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 /**
  * Generated class for the CategoryPage page.
@@ -15,14 +16,68 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoryPage {
 
+  expenses_category: any = [];
+  income_category: any = [];
+
   segmentblock:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public modalCtrl : ModalController,
+    private sqlite: SQLite,) {
     this.segmentblock = "Expenses";
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CategoryPage');
   }
+
+
+
+  getData() {
+    this.sqlite.create({
+      name: 'mebdb.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('CREATE TABLE IF NOT EXISTS expense(categoryid INTEGER PRIMARY KEY, categorytype TEXT, type TEXT,categoryname TEXT)', [])
+        .then(res => console.log('Executed SQL'))
+        .catch(e => console.log(e));
+      db.executeSql('SELECT * FROM expense where categorytype="Expense"  ORDER BY rowid DESC', [])
+        .then(res => {
+          this.expenses_category = [];
+          for (var i = 0; i < res.rows.length; i++) {
+            this.expenses_category.push({ categoryid: res.rows.item(i).categoryid, categorytype: res.rows.item(i).categorytype, categoryname: res.rows.item(i).categoryname})
+          }
+        })
+        .catch(e => console.log(e));
+
+        db.executeSql('SELECT * FROM expense where categorytype="Income"  ORDER BY rowid DESC', [])
+        .then(res => {
+          this.income_category = [];
+          for (var i = 0; i < res.rows.length; i++) {
+            this.income_category.push({ categoryid: res.rows.item(i).categoryid, categorytype: res.rows.item(i).categorytype, categoryname: res.rows.item(i).categoryname})
+          }
+        })
+        .catch(e => console.log(e));
+
+
+    }).catch(e => console.log(e));
+  }
+
+  public openModal(){
+    var data = { pagename : '',datavalue:'' };
+    if(this.segmentblock==="Expenses"){
+      data.pagename="Expenses";
+    }else{
+      data.pagename="Income";
+
+    }
+
+    
+
+    
+    var modalPage = this.modalCtrl.create('AddcategoryPage',data);
+    modalPage.present();
+}
 
 }
